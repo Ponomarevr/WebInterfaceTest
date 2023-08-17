7d82ee611e3b624f047b535eb332e31812141443
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-08-17 15:39:40 +0300

    Readme and Tests updated

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# WebInterfaceTest
-Домашнее задание к занятию «2.1. Тестирование веб-интерфейсов»
+# Домашнее задание к занятию «2.1. Тестирование веб-интерфейсов»
 [![Build status](https://ci.appveyor.com/api/projects/status/nskh845q682gb5mh?svg=true)](https://ci.appveyor.com/project/Ponomarevr/webinterfacetest)
--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -7,23 +7,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderTest {
     private WebDriver driver;
 
-
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
-
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
@@ -32,7 +27,6 @@ class OrderTest {
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
-
     }
 
     @AfterEach
@@ -42,14 +36,12 @@ class OrderTest {
     }
 
     @Test
-    void shouldCorrectFill() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Роман Знаменский");
-        elements.get(1).sendKeys("+72345678910");
+    void shouldCorrectFill() throws InterruptedException {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Пиотр Знаменский-Погонов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+72345678910");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-
     }
 }
